2a6f18a0767b8e07f7000c6a3f541afc3e623f13
Zane <zanebilous@gmail.com>
2022-05-13 22:20:30 -0700

    Clean up theme toggle code

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -1,13 +1,40 @@
-(function () {
-  let cookies = {};
-
-  document.cookie.split(';').forEach(function(el) {
-	let [key,value] = el.split('=');
-	  cookies[key.trim()] = value;
-  });
-
-  if (cookies["theme"]) {
-	console.log(cookies["theme"]);
-	jtd.setTheme(cookies["theme"]);
-  }
+// Initializes the theme based on the theme cookie.
+(function initTheme() {
+  var cookies = document.cookie.split(';');
+	for (const cookie of cookies) {
+		let [key, value] = cookie.split('=');
+		if (key == 'theme') {
+			jtd.setTheme(value);
+		}
+	}
 })();
+
+// Initializes the classes for the theme-toggle icon based on the theme.
+function initThemeToggleIcon() {
+  let themeToggle = document.getElementById('theme-toggle');
+  if (jtd.getTheme() == 'light' || jtd.getTheme() == 'default') {
+    themeToggle.className = 'fa-solid fa-toggle-on';
+  } else {
+    themeToggle.className = 'fa-solid fa-toggle-off';
+  }
+}
+
+// Toggles the theme-toggle icon and updates the theme cookie.
+function toggleThemeToggleIcon() {
+  if (jtd.getTheme() == 'light') {
+    jtd.setTheme('dark');
+  } else {
+    jtd.setTheme('light');
+  }
+
+  document.cookie='theme='+jtd.getTheme()+'; path=/';
+
+  let themeToggle = document.getElementById('theme-toggle');
+  if (themeToggle.className == 'fa-solid fa-toggle-on') {
+    themeToggle.className = 'fa-solid fa-toggle-off';
+  } else {
+    themeToggle.className = 'fa-solid fa-toggle-on';
+  }
+}
+
+
